e7a8e41a3ad6030983b8ffba764b5bb9303fae30
nguyenhao12345 <anhhaonguyen.st@gmail.com>
2018-11-22 16:32:32 +0700

    delete all the feate_x

--- GET-POST/Shipper/HomeShipper/NewScreen/NewScreen/View/NewDetailController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NewDetailController.swift
-//  GET-POST
-//
-//  Created by HaoNguyen on 11/13/18.
-//  Copyright © 2018 datnguyen. All rights reserved.
-//
-
-import Foundation
--- GET-POST/Shipper/HomeShop/CreateNewsItem/ExtensionCreateNewsController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ExtensionCreateNewsController.swift
-//  GET-POST
-//
-//  Created by Nguyen Hieu on 11/8/18.
-//  Copyright © 2018 datnguyen. All rights reserved.
-//
-
-import Foundation
